759c11c4027164f2d5cde64a6e199d4b77909bc8
Nick Dodson <silentcicero@outlook.com>
2016-04-19 00:21:52 -0400

    Update fecha.js

--- fecha.js
+++ fecha.js
@@ -6,7 +6,7 @@
    * @class fecha
    */
   var fecha = {};
-  var token = /d{1,4}|M{1,4}|YY(?:YY)?|S{1,3}|Do|ZZ|([HhMsDm])\1?|[aA]|"[^"]*"|'[^']*'/g;
+  var token = /d{1,4}|M{1,4}|YY(?:YY)?|S{1,3}|Do|ZZ|x|X|([HhMsDm])\1?|[aA]|"[^"]*"|'[^']*'/g;
   var twoDigits = /\d\d?/;
   var threeDigits = /\d{3}/;
   var fourDigits = /\d{4}/;
