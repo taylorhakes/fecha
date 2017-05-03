b26ca32ce86154a55444c879677d3e57c5ad403f
Goh Yisheng Andrew <mail.yisheng@gmail.com>
2017-05-03 14:49:54 +0800

    Wrap types in Fecha namespace.

--- fecha.d.ts
+++ fecha.d.ts
@@ -1,47 +1,51 @@
-type Days = [string, string, string, string, string, string, string];
-type Months = [
-  string,
-  string,
-  string,
-  string,
-  string,
-  string,
-  string,
-  string,
-  string,
-  string,
-  string,
-  string
-];
+declare namespace Fecha {
+    type Days = [string, string, string, string, string, string, string];
+    type Months = [
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      string,
+      string
+    ];
 
-export interface i18nSettings {
-  amPm: [string, string],
+    interface i18nSettings {
+      amPm: [string, string],
 
-  dayNames: Days,
+      dayNames: Days,
 
-  dayNamesShort: Days,
+      dayNamesShort: Days,
 
-  monthNames: Months,
+      monthNames: Months,
 
-  monthNamesShort: Months,
+      monthNamesShort: Months,
 
-  DoFn: (D: number) => string
+      DoFn: (D: number) => string
+    }
+
+    export var masks: {
+      default: string,
+      fullDate: string,
+      longDate: string,
+      longTime: string,
+      mediumDate: string,
+      mediumTime: string,
+      shortDate: string,
+      shortTime: string,
+      [myMask: string]: string
+    };
+
+    export var i18n: i18nSettings;
+
+    export function format(dateObj: Date | number, mask: string, i18nSettings?: i18nSettings): string;
+
+    export function parse(dateStr: string, format: string, i18nSettings?: i18nSettings): Date;
 }
 
-export var masks: {
-  default: string,
-  fullDate: string,
-  longDate: string,
-  longTime: string,
-  mediumDate: string,
-  mediumTime: string,
-  shortDate: string,
-  shortTime: string,
-  [myMask: string]: string
-};
-
-export var i18n: i18nSettings;
-
-export function format(dateObj: Date | number, mask: string, i18nSettings?: i18nSettings): string;
-
-export function parse(dateStr: string, format: string, i18nSettings?: i18nSettings): Date;
+export = Fecha
