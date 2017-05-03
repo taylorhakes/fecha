b60ca0d69793bf975e320f2e00234f15de3055d5
Goh Yisheng Andrew <mail.yisheng@gmail.com>
2017-05-03 14:14:52 +0800

    Add typescript declaration file.

---
+++ fecha.d.ts
@@ -0,0 +1,47 @@
+type Days = [string, string, string, string, string, string, string];
+type Months = [
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string
+];
+
+export interface i18nSettings {
+  amPm: [string, string],
+
+  dayNames: Days,
+
+  dayNamesShort: Days,
+
+  monthNames: Months,
+
+  monthNamesShort: Months,
+
+  DoFn: (D: number) => string
+}
+
+export var masks: {
+  default: string,
+  fullDate: string,
+  longDate: string,
+  longTime: string,
+  mediumDate: string,
+  mediumTime: string,
+  shortDate: string,
+  shortTime: string,
+  [myMask: string]: string
+};
+
+export var i18n: i18nSettings;
+
+export function format(dateObj: Date | number, mask: string, i18nSettings?: i18nSettings): string;
+
+export function parse(dateStr: string, format: string, i18nSettings?: i18nSettings): Date;
