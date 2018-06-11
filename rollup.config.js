8db39d4c0a5e769f6fa8ec0c8483f18924660df9
Paulus Schoutsen <balloob@gmail.com>
2018-06-11 15:10:28 -0400

    Re-organize files

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,47 @@
 import uglify from 'rollup-plugin-uglify';
 
-const plugins = [];
+const browserPlugins = [];
 if (process.env.NODE_ENV === 'production') {
-  plugins.push(uglify());
+  browserPlugins.push(uglify());
 }
 
 export default [
+  // Compressed (for direct consumption in browser)
+  // written to dist folder, minified.
   {
-    input: 'fecha.js',
-    plugins: plugins,
+    input: 'src/fecha.js',
+    plugins: browserPlugins,
     output: {
       // How it will be exposed on window
       name: 'fecha',
       format: 'umd',
-      file: 'fecha.min.js',
+      file: 'dist/fecha.min.js',
     }
   },
   {
-    input: 'fecha.strict.js',
-    plugins: plugins,
+    input: 'src/fecha.strict.js',
+    plugins: browserPlugins,
     output: {
-      // How it will be exposed on window
       name: 'fecha',
       format: 'umd',
-      file: 'fecha.strict.min.js',
+      file: 'dist/fecha.strict.min.js',
+    }
+  },
+  // For Node: no minify, output in lib dir
+  {
+    input: 'src/fecha.js',
+    output: {
+      name: 'fecha',
+      format: 'umd',
+      file: 'lib/fecha.umd.js',
+    }
+  },
+  {
+    input: 'src/fecha.strict.js',
+    output: {
+      name: 'fecha',
+      format: 'umd',
+      file: 'lib/fecha.strict.umd.js',
     }
   },
 ];
